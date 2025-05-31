4a5cce04192e9dc3d64d0b429be8107d94c6bab6
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-05-31 20:16:14 +0200

    fix some misunderstandings in src/validation/contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -9,7 +9,7 @@ export const createContactSchema = Joi.object({
 }),
 
     phoneNumber: Joi.string().required(),
-    email: Joi.string().email().min(3).max(20),
+    email: Joi.string().email(),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home', 'personal').required(),
     userId: Joi.string(),
@@ -19,7 +19,7 @@ export const createContactSchema = Joi.object({
 export const updateContactSchema = Joi.object({
     name: Joi.string().min(3).max(20),
     phoneNumber: Joi.string(),
-    email: Joi.string().email().min(3).max(20),
+    email: Joi.string().email(),
     isFavourite: Joi.boolean(),
     contactType: Joi.string().valid('work', 'home', 'personal'),
     photo:Joi.string(),
